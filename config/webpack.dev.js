7f7f21b81fe581330f9c4c16e73d70ec9b2718d5
JackWSargent <jackwsargent@gmail.com>
2020-07-31 23:37:25 -0400

    Added less, sass, stlylus

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,86 +1,125 @@
-const path = require("path")
-const webpack = require("webpack")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: ["./src/main.js"]
-  },
-  output: {
-    filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
-  },
-  devServer: {
-    contentBase: "dist",
-    overlay: true,
-    stats: {
-      colors: true
-    }
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      {
-        test: /\.jpg$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "images/[name]-[hash:4].[ext]"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.pug$/,
-        use: [
-          {
-            loader: "pug-loader"
-          }
-        ]
-      },
-      {
-        test: /\.ejs$/,
-        use: [
-          {
-            loader: "ejs-loader"
-          }
-        ]
-      },
-      {
-        test: /\.hbs$/,
-        use: [
-          {
-            loader: "handlebars-loader",
-            query: {
-              inlineRequires: `\/images\/`
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // Enable HMR
-    new webpack.NamedModulesPlugin(),
-    new HTMLWebpackPlugin({
-      template: "./src/index.ejs",
-      inject: true,
-      title: "Link's Journal"
-    })
-  ]
-}
+    entry: {
+        main: ["./src/main.js"],
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist"),
+        publicPath: "/",
+    },
+    devServer: {
+        contentBase: "dist",
+        overlay: true,
+        hot: true,
+        stats: {
+            colors: true,
+        },
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        // query: {
+                        //     modules: true,
+                        //     localIdentName: "[name]--[local]",
+                        // },
+                    },
+                ],
+            },
+            {
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "images/[name].[ext]",
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.ejs$/,
+                use: [
+                    {
+                        loader: "ejs-loader",
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: "pug-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    {
+                        loader: "handlebars-loader",
+                        query: {
+                            inlineRequires: `\/images\/`,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.sass$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }],
+            },
+            {
+                test: /\.less$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "less-loader" }],
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "postcss-loader" },
+                    { loader: "stylus-loader" },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            test: /\.hbs$/,
+            use: [
+                {
+                    loader: "handlebars-loader",
+                    query: { inlineRequires: "/images/" },
+                },
+            ],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: "./src/index.ejs",
+            inject: true,
+            title: "Link's Journal",
+        }),
+    ],
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-require("babel-runtime/regenerator")
-require("babel-core/register")
-require("webpack-hot-middleware/client?reload=true")
-require("./main.css")
-require("./images/link.jpg")
-require("./index.ejs")
+require("babel-runtime/regenerator");
+require("babel-core/register");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.styl");
+require("./images/link.jpg");
+require("./index.ejs");
